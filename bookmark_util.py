6f6f8668c26ee6117ab1213a0f085c34839553a7
Yuya Kato <yuyakato@gmail.com>
2009-11-24 16:27:27 +0900

    リファクタリング

--- bookmark_util.py
+++ bookmark_util.py
@@ -3,15 +3,16 @@
 import re
 
 class BookmarkUtil:
+  REJECT_SITES = [
+    re.compile(r"http://car\.nikkei\.co\.jp/release/"), # 理由: title要素に記事タイトルを含まないため
+    re.compile(r"http://markets\.nikkei\.co\.jp/"),     # 理由: title要素に記事タイトルを含まないため
+    re.compile(r"http://www\.nikkei\.co\.jp/news/"),    # 理由: title要素に記事タイトルを含まないため
+    re.compile(r"http://www\.pjnews\.net/"),            # 理由: title要素に記事タイトルを含まないため
+  ]
+
   @classmethod
   def is_rejected_site(cls, url):
-    patterns = [
-      re.compile(r"http://car\.nikkei\.co\.jp/release/"), # 理由: title要素に記事タイトルを含まないため
-      re.compile(r"http://markets\.nikkei\.co\.jp/"),     # 理由: title要素に記事タイトルを含まないため
-      re.compile(r"http://www\.nikkei\.co\.jp/news/"),    # 理由: title要素に記事タイトルを含まないため
-      re.compile(r"http://www\.pjnews\.net/"),            # 理由: title要素に記事タイトルを含まないため
-    ]
-    for pattern in patterns:
+    for pattern in cls.REJECT_SITES:
       if pattern.match(url):
         return True
     return False
