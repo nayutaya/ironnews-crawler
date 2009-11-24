c20614483d0e1bc6b2f484882daf7e137cac4727
Yuya Kato <yuyakato@gmail.com>
2009-11-24 16:22:52 +0900

    ブラックリストに載っているサイトをデータストアに追加しないように変更

--- crawler_google_news.py
+++ crawler_google_news.py
@@ -8,6 +8,8 @@ from article_manager import ArticleManager
 from keyword_manager import KeywordManager
 from google_news import GoogleNews
 
+from bookmark_util import BookmarkUtil
+
 print "Content-Type: text/plain"
 print ""
 
@@ -20,8 +22,12 @@ articles = GoogleNews.search(keyword, 30)
 for article in articles:
   url   = article["url"]
   title = article["title"]
+  print "---"
   print url
   print title.encode("utf-8")
-  ArticleManager.add(url, title, Article.CATEGORY_RAIL)
+  if BookmarkUtil.is_rejected_site(url):
+    print "reject!"
+  else:
+    ArticleManager.add(url, title, Article.CATEGORY_RAIL)
 
 KeywordManager.update(keyword)
