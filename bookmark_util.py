49d4b0f873159ffbc0a0a37ae7a05f225f075191
Yuya Kato <yuyakato@gmail.com>
2009-11-24 16:24:48 +0900

    ブラックリストを更新

--- bookmark_util.py
+++ bookmark_util.py
@@ -6,10 +6,10 @@ class BookmarkUtil:
   @classmethod
   def is_rejected_site(cls, url):
     patterns = [
-      re.compile(r"http://www\.nikkei\.co\.jp/news/"),        # 理由: title要素に記事タイトルを含まないため
-      re.compile(r"http://markets\.nikkei\.co\.jp/kokunai/"), # 理由: title要素に記事タイトルを含まないため
-      re.compile(r"http://car\.nikkei\.co\.jp/release/"),     # 理由: title要素に記事タイトルを含まないため
-      re.compile(r"http://www\.pjnews\.net/"),                # 理由: title要素に記事タイトルを含まないため
+      re.compile(r"http://car\.nikkei\.co\.jp/release/"), # 理由: title要素に記事タイトルを含まないため
+      re.compile(r"http://markets\.nikkei\.co\.jp/"),     # 理由: title要素に記事タイトルを含まないため
+      re.compile(r"http://www\.nikkei\.co\.jp/news/"),    # 理由: title要素に記事タイトルを含まないため
+      re.compile(r"http://www\.pjnews\.net/"),            # 理由: title要素に記事タイトルを含まないため
     ]
     for pattern in patterns:
       if pattern.match(url):
