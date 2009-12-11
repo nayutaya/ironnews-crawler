8115edf3ea87c6f4f039e4fcec820eeef7129afd
Yuya Kato <yuyakato@gmail.com>
2009-12-11 17:21:45 +0900

    「googleニュース」タグを付加する処理を追加

--- registration.rb
+++ registration.rb
@@ -14,20 +14,28 @@ username, password = File.open("registration.id") { |file| [file.gets.chomp, fil
 #host = "localhost:8080"
 host = "ironnews-crawler1.appspot.com"
 
-get_url = "http://#{host}/api/get-unregistered-articles"
-json    = open(get_url) { |io| io.read }
+10.times {
+  get_url = "http://#{host}/api/get-unregistered-articles"
+  json    = open(get_url) { |io| io.read }
 
-articles = JSON.parse(json)
-articles.sort_by { rand }.each { |article|
-  p article
-  url = article["url"]
-  canonical_url = BookmarkUtility.get_canonical_url(url)
-  add_url = "http://ironnews.nayutaya.jp/api/add_article?url1=" + CGI.escape(canonical_url)
-  wsse    = Wsse::UsernameToken.build(username, password).format
-  open(add_url, {"X-WSSE" => wsse}) { |io| io.read }
+  articles = JSON.parse(json)
+  articles.sort_by { rand }.each { |article|
+    p article
+    url = article["url"]
+    canonical_url = BookmarkUtility.get_canonical_url(url)
+    add_url = "http://ironnews.nayutaya.jp/api/add_article?url1=" + CGI.escape(canonical_url)
+    wsse    = Wsse::UsernameToken.build(username, password).format
+    json    = open(add_url, {"X-WSSE" => wsse}) { |io| io.read }
+    ret     = JSON.parse(json)
+    article_id = ret["result"]["1"]["article_id"]
 
-  set_url = "http://#{host}/api/set-registered-article?url=" + CGI.escape(url)
-  open(set_url) { |io| io.read }
+    tag_url = "http://ironnews.nayutaya.jp/api/add_tags?article_id=#{article_id}&tag1=" + CGI.escape("googleニュース")
+    wsse    = Wsse::UsernameToken.build(username, password).format
+    open(tag_url, {"X-WSSE" => wsse}) { |io| io.read }
 
-  sleep(0.5)
+    set_url = "http://#{host}/api/set-registered-article?url=" + CGI.escape(url)
+    open(set_url) { |io| io.read }
+
+    sleep(0.5)
+  }
 }
