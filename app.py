5ffcc00efb0fa62afc488aba96800da86848ca01
Yuya Kato <yuyakato@gmail.com>
2009-12-11 15:44:29 +0900

    未登録記事を返すAPIの雛形を追加

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append("lib")
+
+import simplejson
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+
+from article_manager import Article
+from article_manager import ArticleManager
+
+class GetUnregisteredArticles(webapp.RequestHandler):
+  def get(self):
+    callback = self.request.get("callback")
+
+    result = {}
+
+    json = simplejson.dumps(result, separators=(',',':'))
+    if callback != "": json = callback + "(" + json + ")"
+    self.response.headers["Content-Type"] = "text/javascript"
+    self.response.out.write(json)
 
 class HomePage(webapp.RequestHandler):
   def get(self):
@@ -10,6 +28,7 @@ class HomePage(webapp.RequestHandler):
 if __name__ == "__main__":
   application = webapp.WSGIApplication(
     [
+      (r"/api/get-unregistered-articles", GetUnregisteredArticles),
       (r"/", HomePage),
     ],
     debug = True)
