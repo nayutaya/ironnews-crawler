67a04f3c147a5cd554da7da24f1ab48b7e025930
Yuya Kato <yuyakato@gmail.com>
2009-12-17 13:07:13 +0900

    登録済みの古い記事（30日以上前）を削除する処理を追加

--- cleaner.py
+++ cleaner.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+from google.appengine.ext import db
+from article_manager import Article
+
 print "Content-Type: text/plain"
 print ""
 
 print "cleaner"
+
+now    = datetime.datetime.now()
+expire = now - datetime.timedelta(days = 30)
+
+print "now    : " + now.strftime("%Y-%m-%d %H:%M:%S")
+print "expire : " + expire.strftime("%Y-%m-%d %H:%M:%S")
+
+articles = db.GqlQuery("SELECT * FROM Article WHERE state = :1 AND created_at < :2",
+  Article.STATE_REGISTERED, expire)
+
+print "found  : " + str(articles.count())
+
+db.delete(articles)
+
+print "clean"
