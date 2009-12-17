266f29a91e7f48881370480ecfcda26bb35ad4a1
Yuya Kato <yuyakato@gmail.com>
2009-12-17 13:16:15 +0900

    一度に削除する件数を200レコードに制限
    * Google App Engineの制約で、500レコードまでしか一括削除できないため

--- cleaner.py
+++ cleaner.py
@@ -15,7 +15,7 @@ expire = now - datetime.timedelta(days = 30)
 print "now    : " + now.strftime("%Y-%m-%d %H:%M:%S")
 print "expire : " + expire.strftime("%Y-%m-%d %H:%M:%S")
 
-articles = db.GqlQuery("SELECT * FROM Article WHERE state = :1 AND created_at < :2",
+articles = db.GqlQuery("SELECT * FROM Article WHERE state = :1 AND created_at < :2 LIMIT 200",
   Article.STATE_REGISTERED, expire)
 
 print "found  : " + str(articles.count())
